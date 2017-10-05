5a2a4d44588c0c089959d5ff978ccbc3c1ddf202
Matthew Kruk <matthew.kruk@gmail.com>
2017-10-05 13:58:46 -0400

    add insertedCoinHold / test refactor

--- src/main/java/io/krukm/model/VendingMachine.java
+++ src/main/java/io/krukm/model/VendingMachine.java
@@ -1,10 +1,13 @@
 package io.krukm.model;
 
 
+import java.util.Stack;
+
 public class VendingMachine {
 
     Display display = new Display();
     CoinReserve coinReserve = new CoinReserve();
+    Stack<Coin> insertedCoinHold = new Stack<>();
 
 
     VendingMachine() {
@@ -13,8 +16,9 @@ public class VendingMachine {
 
     public void insertCoin(Coin coin) {
         if (coinReserve.coinAccepted(coin)) {
-            coinReserve.addCoin(coin);
-            display.setMessage(5, coin.value);
+            insertedCoinHold.add(coin);
+            display.setMessage(5, coinReserve.stackTotal(insertedCoinHold));
         }
     }
+
 }
--- src/test/java/io/krukm/model/VendingMachineTest.java
+++ src/test/java/io/krukm/model/VendingMachineTest.java
@@ -2,6 +2,8 @@ package io.krukm.model;
 
 import org.junit.Test;
 
+import java.util.Stack;
+
 import static junit.framework.TestCase.assertEquals;
 
 public class VendingMachineTest {
@@ -17,5 +19,15 @@ public class VendingMachineTest {
     public void whenInsertingCoinsDisplayShouldShowTotalAmountInsertedSoFar() {
         vendingMachine.insertCoin(Coin.COIN_FOUR);
         assertEquals("0.25", vendingMachine.display.getMessage());
+    }
+
+    @Test
+    public void whenInsertedCoinsShouldBeAddedToTemporaryHoldUntilProductSelected() {
+        Stack<Coin> coins = new Stack<>();
+        coins.add(Coin.COIN_FOUR);
+        coins.add(Coin.COIN_THREE);
+        vendingMachine.insertCoin(Coin.COIN_FOUR);
+        vendingMachine.insertCoin(Coin.COIN_THREE);
+        assertEquals(coins, vendingMachine.insertedCoinHold);
     }
 }
