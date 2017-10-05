c2073bac9af7fadd69efd302eb9d75b05b0f117b
Matthew Kruk <matthew.kruk@gmail.com>
2017-10-05 13:06:52 -0400

    add CoinReserve/ test

---
+++ src/main/java/io/krukm/model/CoinReserve.java
@@ -0,0 +1,29 @@
+package io.krukm.model;
+
+import java.util.Stack;
+
+public class CoinReserve {
+
+    Stack<Coin> nickelStack = new Stack<>();
+    Stack<Coin> dimeStack = new Stack<>();
+    Stack<Coin> quarterStack = new Stack<>();
+
+    public boolean coinAccepted(Coin coin) {
+
+        switch (coin) {
+            case PENNY:
+                return false;
+            case NICKEL:
+                nickelStack.push(Coin.NICKEL);
+                return true;
+            case DIME:
+                dimeStack.push(Coin.DIME);
+                return true;
+            case QUARTER:
+                quarterStack.push(Coin.QUARTER);
+                return true;
+        }
+        if (coinAccepted(coin)) return true;
+        else return false;
+    }
+}
---
+++ src/test/java/io/krukm/model/CoinReserveTest.java
@@ -0,0 +1,48 @@
+package io.krukm.model;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class CoinReserveTest {
+    private CoinReserve coinReserve = new CoinReserve();
+
+    @Test
+    public void rejectPennyWhenAdded() {
+        assertFalse(coinReserve.coinAccepted(Coin.PENNY));
+    }
+
+    @Test
+    public void acceptNickelWhenAdded() {
+        assertTrue(coinReserve.coinAccepted(Coin.NICKEL));
+    }
+
+    @Test
+    public void acceptDimeWhenAdded() {
+        assertTrue(coinReserve.coinAccepted(Coin.DIME));
+    }
+
+    @Test
+    public void acceptQuarterWhenAdded() {
+        assertTrue(coinReserve.coinAccepted(Coin.QUARTER));
+    }
+
+    @Test
+    public void addNickelToNickelStackWhenAccepted() {
+        coinReserve.coinAccepted(Coin.NICKEL);
+        assertFalse(coinReserve.nickelStack.empty());
+    }
+
+    @Test
+    public void addDimeToDimeStackWhenAccepted() {
+        coinReserve.coinAccepted(Coin.DIME);
+        assertFalse(coinReserve.dimeStack.empty());
+    }
+
+    @Test
+    public void addQuarterToQuarterStackWhenAccepted() {
+        coinReserve.coinAccepted(Coin.QUARTER);
+        assertFalse(coinReserve.quarterStack.empty());
+    }
+}
